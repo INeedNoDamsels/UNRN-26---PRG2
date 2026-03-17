29c207aba241fb78cc5d70d99e40f593d52737a1
Manu Bernabei <manubernabei26@gmail.com>
2026-03-17 14:41:27 -0300

    cpp del ejercicio 1 terminado

--- TP1/ejercicio1.cpp
+++ TP1/ejercicio1.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cmath>
 
+const int costos[2] = {10, 13};
+
 int main(int argc, char* argv[]) {
     float terreno[2];
     float valores[2];
@@ -15,12 +17,11 @@ int main(int argc, char* argv[]) {
         terreno[1] = atoi(argv[2]);
     }
 
-    valores[0] = (terreno[0] + terreno[1]) * 10;
-    valores[1] = sqrt(pow(terreno[0], 2) + pow(terreno[1], 2)) * 18;
+    valores[0] = costos[0] * (terreno[0] + terreno[1]);
+    valores[1] = costos[1] * sqrt(pow(terreno[0], 2) + pow(terreno[1], 2));
 
     std::cout << " < La opcion mas economica seria cablear ";
-    std::cout << (valores[0] < valores[1] ? "por el borde" : "en diagonal");
-    std::cout << " ya que costaria $" << valores[0] << " frente a $" << valores[1];
+    std::cout << (valores[0] < valores[1] ? "por el borde.\n" : "en diagonal.\n");
 
     return 0;
 }
