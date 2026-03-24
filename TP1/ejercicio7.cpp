9255e8c7e9b4cf1575d640ac539be06ad5057848
Manu Bernabei <manubernabei26@gmail.com>
2026-03-24 13:53:54 -0300

    Finalización del ejercicio 7 (.cpp)

--- TP1/ejercicio7.cpp
+++ TP1/ejercicio7.cpp
@@ -6,40 +6,32 @@
 #include <thread>
 #include <chrono>
 
-class Producto {
-public:
-    std::string nombre;
-    float precioUnitario;
+int ingresar() {
+    int valor;
 
-    Producto(std::string n, float p) : nombre(n), precioUnitario(p) {}
-};
+    while (true) {
+        std::string input;
 
-class Inventario {
-public:
-    Producto producto;
-    unsigned cantidad, puntoCritico;
+        std::cout << "<1> Actualizar stock <2> Realizar pedido\n\t\t<Otro> Vender\n\t\t  <3> Salir" << std::endl;
+        std::cout << "\n > Ingrese opcion: "; std::cin >> input;
 
-    Inventario(const Producto& p, unsigned c, unsigned pc) : producto(p), cantidad(c), puntoCritico(pc) {}
-
-    bool necesitaReposicion() const {
-        return cantidad <= puntoCritico;
+        try {
+            valor = std::stoi(input);
+            if (valor <= 0) throw std::invalid_argument("");
+            return valor;
+        } catch (...) {
+            for (int i = 0; i < 19 ; i++) std::cout << " ";
+            std::cout << "^ Error: ingreso invalido.\n" << std::endl;
+        }
     }
-};
-
-void actualizarStock(Inventario& inventario) {
-    std::cout << " < " << inventario.producto.nombre << ", quedan: " << inventario.cantidad;
-
-    if (!inventario.necesitaReposicion()) std::cout << ". No necesita reposicion." << std::endl;
 }
-
-float reponerStock(Inventario& inventario, unsigned cantidad, float costo) {
-    inventario.cantidad += cantidad;
-
-    if (cantidad > 50) costo = 0.0f;
-
-    return costo + (cantidad * inventario.producto.precioUnitario);
+void lapso(int segundos) {
+    for (int i = 0; i < segundos; i++) {
+        std::cout << ".";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    std::cout << std::endl << std::endl;
 }
-
 int obtener(int min, int max) {
     static std::random_device semilla;
     static std::mt19937 valor(semilla());
@@ -48,61 +40,140 @@ int obtener(int min, int max) {
     return generador(valor);
 }
 
-void lapso(int segundos) {
-    for (int i = 0; i < segundos; i++) {
-        std::cout << ".";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+class Producto {
+public:
+    std::string nombre;
+    float precioUnitario;
+
+    Producto(std::string n, float p) : nombre(n), precioUnitario(p) {}
+};
+class Inventario {
+public:
+    Producto producto;
+    int cantidad, puntoCritico;
+
+    Inventario(const Producto& p, int c, int pc) : producto(p), cantidad(c), puntoCritico(pc) {}
+
+    bool necesitaReposicion() const {
+        return cantidad <= puntoCritico;
     }
-    std::cout << std::endl << std::endl;
+
+    void mostrar() const {
+        std::cout << " < " << producto.nombre << ", quedan: " << cantidad;
+
+        if (necesitaReposicion()) std::cout << ". Necesita reposicion.";
+
+        std::cout << std::endl;
+    }
+
+    float reponer(int cantidad) {
+        this->cantidad += cantidad;
+
+        float costo = 100.0f;
+        if (cantidad > 50) {
+            costo = 0.0f;
+            std::cout << " < Costo fijo bonificado." << std::endl;
+        }
+
+        return costo + (cantidad * producto.precioUnitario);
+    }
+};
+class Negocio {
+public:
+    float saldo;
+    float deuda;
+    Inventario items[3];
+
+    Negocio(float s, Inventario* i) : saldo(s), deuda(0.0f), items{i[0], i[1], i[2]} {}
+
+    void vender() {
+    int i = obtener(0, 2);
+
+    if (items[i].cantidad > 0) {
+        int vendidas = obtener(1, items[i].cantidad);
+
+        float precioVenta = items[i].producto.precioUnitario * 1.3f;
+        float ganancia = vendidas * precioVenta;
+
+        items[i].cantidad -= vendidas;
+
+        std::cout << " > Vendiste " << vendidas << " de " << items[i].producto.nombre << " | Ganancia: $" << ganancia << std::endl;
+
+        if (deuda > 0) {
+            if (ganancia >= deuda) {
+                ganancia -= deuda;
+                deuda = 0;
+                saldo += ganancia;
+            } else {
+                deuda -= ganancia;
+            }
+        } else saldo += ganancia;
+    } else std::cout << " < No hay stock para vender." << std::endl;
 }
 
-int ingresar() {
-    int valor;
+    void mostrarStock() const {
+        std::cout << "[Saldo: $" << saldo << " | Deuda: $" << deuda << "]" << std::endl;
 
-    while (true) {
-        std::string input;
-        std::cout << "<1> Actualizar stock " << "<2> Realizar pedido" << "\n\t<3> Salir" << std::endl;
-        std::cout << "\n > Ingrese opcion: "; std::cin >> input;
+        for (int i = 0; i < 3; i++) items[i].mostrar();
+    }
 
-        try {
-            valor = std::stoi(input);
-            if (valor <= 0) throw std::invalid_argument("");
-            return valor;
-        } catch (...) {
-            for (int i = 0; i < 19; i++) std::cout << " ";
-            std::cout << "^ Error: ingreso invalido.\n\n";
+    void realizarPedido() {
+        float total = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (items[i].necesitaReposicion()) {
+                int cantidad = obtener(30, 60);
+                total += items[i].reponer(cantidad);
+            }
+        }
+
+        std::cout << " < Costo total: $" << total << std::endl;
+
+        saldo -= total;
+
+        if (saldo < 0) {
+            deuda += -saldo;
+            saldo = 0;
         }
     }
-}
+};
 
 int main() {
+    bool bandera = true;
+
     Producto productos[3] = {
-        Producto("Pan", 9.5f),
-        Producto("Leche", 14.6f),
-        Producto("Huevos", 18.0f)
+        {"Pan", 9.5f},
+        {"Leche", 14.6f},
+        {"Huevos", 18.0f}
     };
-
     Inventario items[3] = {
-        Inventario(productos[0], obtener(5 ,30), 4),
-        Inventario(productos[1], obtener(5, 30), 4),
-        Inventario(productos[2], obtener(5, 30), 4)
+        {productos[0], obtener(5, 30), 4},
+        {productos[1], obtener(5, 30), 4},
+        {productos[2], obtener(5, 30), 4}
     };
+    Negocio miNegocio(100.0f, items);
 
-    while (true) {
-        std::cout << "\n - Programa de gestion de stock -" << std::endl;
+    while (bandera) {
+        std::cout << "\n      - Programa de gestion de stock -" << std::endl;
         switch (ingresar()) {
             case 1:
-                std::cout << "  < Revisando stock"; lapso(3);
-                for (int i = 0; i < 3; i++) actualizarStock(items[i]);
+                std::cout << " < Revisando stock"; lapso(3);
+                miNegocio.mostrarStock();
                 break;
 
             case 2:
-                std::cout << "  < Realizando pedido"; lapso(3);
-                for (int i = 0; i < 3; i++) reponerStock(items[i], obtener(30, 60), items[i].producto.precioUnitario);
+                std::cout << " < Realizando pedido"; lapso(3);
+                miNegocio.realizarPedido();
+                break;
+
+            case 3:
+                std::cout << " < Cerrando sesion"; lapso(3);
+                bandera = false;
                 break;
 
             default:
-                //
+                std::cout << " < Atendiendo clientes"; lapso(3);
+                if (obtener(1, 9) % 2 == 0) miNegocio.vender();
                 break;
         }
     }
